bf6c09b20eda160b7f18c98ad267830b6efd6cfb
Yiheng <development.henryquan@gmail.com>
2023-09-09 21:05:38 +1000

    improved the accurate with repeats maybe...

--- resume/resume_ai.py
+++ resume/resume_ai.py
@@ -17,15 +17,36 @@ def print(*args, **kwargs):
         builtins.print(*args, **kwargs)
 
 
+class ResumeInfo:
+    def __init__(
+        self, resume: str, job_post: str, additional: str = "", instruction: str = ""
+    ) -> None:
+        self.resume = resume
+        self.job_post = job_post
+        self.additional = additional
+        self.instruction = instruction
+
+    def generate_prompt(self, role_description: str) -> str:
+        """
+        Generate the prompt for the AI
+        """
+        return (
+            f"Instruction: {self.instruction}\n###\n{role_description}\n###\n"
+            + f"Resume:\n{self.resume}\n###\n"
+            + f"Job post:\n{self.job_post}\n###\n{self.additional}"
+        )
+
+
 class ResumeAIRole(Enum):
     HR = 0
     APPLICANT = 1
 
     def description(self):
+        # Modify the role can result in significant changes in the output
         if self == ResumeAIRole.HR:
-            return "You are the principal HR (Human Resources)/recruiter of a company."
+            return "You are the principal HR (Human Resources)/recruiter of a company reviewing candidates for a new position. Any information which is not relevant to the job post should be ignored. Anything which is not mentioned in the resume should be considered as false."
         elif self == ResumeAIRole.APPLICANT:
-            return "You are a candidate applying for a new job."
+            return "You are a candidate applying for a new job position and working on the resume. Refrain from adding any fake/not given information in the resume. Avoid stating any information which it not true or relevant to the job post."
         raise ValueError("Invalid role")
 
 
@@ -54,58 +75,62 @@ class ResumeAI:
         print("OpenAI token used:", response["usage"]["total_tokens"])
         return output.strip()
 
-    def review(self, resume: str, job_post: str) -> str:
-        """
-        Review a resume from different perspectives
-        """
-        role_description = self._role.description()
-
-        final_message = (
-            self._build_message(role_description, resume, job_post) + "###\n"
-        )
-        response = self._ask_turbo(final_message)
-        return self._retrieve_output(response)
-
-    def score(self, resume: str, job_post: str) -> str:
+    def score_and_review(self, info: ResumeInfo) -> str:
         """
         Rate the resume based on the job post depending on closely the resume matches the job post
         """
-        role_description = self._role.description()
-
-        final_message = (
-            self._build_message(role_description, resume, job_post)
-            + "Based on the give information, how would you rate this resume based on how relavent with the job post? (1 - 100)\n"
-            + "Only output the number without any other text."
+        info.additional = (
+            "Review the resume from your perspective. Give feedback, suggestions or improvements. "
+            + "Indicate if the resume represents a good match with the job post."
+            + "Output the score (0 - 10), review (one summary) and fit (true/false) in the format: Score: <score>\nReview: <review>\nFit: <fit> without any other text."
         )
-        response = self._ask_turbo(final_message)
-        return self._retrieve_output(response)
+        info.instruction = (
+            "The review should be based on how closely the job post and the resume match."
+            + "Note that there is either a good match or not. There is no in-between."
+        )
+        result = self.custom(info)
+        return result
 
-    def rewrite(self, resume: str, job_post: str) -> str:
+    def score_and_review_avg(self, info: ResumeInfo, repeat: int = 3) -> str:
+        """
+        Average the score and review with multiple runs to hopefully get a more accurate result
+        """
+        results = []
+        for _ in range(repeat):
+            result = self.score_and_review(info)
+            print(result)
+            results.append(result)
+        # use it as the input to get the final result
+        final_message = (
+            "\n".join(results)
+            + "\n###\nWith the above results, output the final score (0 - 10), review (one summary) and fit (true/false) in the format: Score: <score>\nReview: <review>\nFit: <fit> without any other text."
+        )
+        final_result = self._retrieve_output(self._ask_turbo(final_message))
+        return final_result
+
+    def rewrite(self, info: ResumeInfo) -> str:
         """
         Rewrite the resume based on the job post
         """
-        role_description = self._role.description()
-
-        final_message = (
-            self._build_message(role_description, resume, job_post)
-            + "Rewrite the resume based on the job post if there is a match.\nGenerate the new resume in a clear format without following its original style.\nApply proper spacing and indentation.\n"
+        info.additional = (
+            "Rewrite the resume and cherrypick things matching the job post to get as close as the posting.\nGenerate the new resume in a clear format.\nApply proper spacing and indentation.\n"
+            + "Add a cover letter highlighting why this resume is a good match for the job post.\n"
             + "Output in the MarkDown format without any other text starting with # Resume"
         )
-        response = self._ask_turbo(final_message)
-        markdown = self._retrieve_output(response)
-        with open("resume/new_resume.md", "w", encoding="utf-8") as f:
-            f.write(markdown)
-        return markdown
 
-    def _build_message(self, role_description: str, resume: str, job_post: str) -> str:
-        """
-        Build the message to send to the AI
-        """
-        language_detection = (
-            "Identify the text language of the resume section. Language means English, Chinese, Japanese or any spoken languages.\n"
-            + "Update the resume's text language (English, Chinese, etc.) to match the job post's language. Output the using result in the job post's text language\n###\n"
-        )
-        return f"{language_detection}{role_description}\n###\nResume:\n{resume}\n###\nJob post:\n{job_post}\n###\n"
+        return self.custom_output(info, "resume/new_resume.md")
+
+    def custom(self, info: ResumeInfo) -> str:
+        role_description = self._role.description()
+        final_message = info.generate_prompt(role_description)
+        response = self._ask_turbo(final_message)
+        return self._retrieve_output(response)
+
+    def custom_output(self, info: ResumeInfo, file_name: str) -> str:
+        output = self.custom(info)
+        with open(file_name, "w", encoding="utf-8") as f:
+            f.write(output)
+        return output
 
     def _ask_turbo(self, message: str) -> dict:
         """
@@ -138,7 +163,7 @@ class ResumeAI:
 if __name__ == "__main__":
     from resume_extract import extract_resume
 
-    resume = ResumeAI(ResumeAIRole.APPLICANT)
+    resume = ResumeAI(ResumeAIRole.HR)
     # print(resume.ask("おなまえは？"))
     # exit()
 
@@ -148,10 +173,8 @@ if __name__ == "__main__":
     with open("resume/job_post.txt", encoding="utf-8") as f:
         job_post = f.read()
     job_post = resume.shrink_input(job_post)
-    # print(resume.review(my_resume, job_post))
-
-    # # score
-    # print(resume.score(my_resume, job_post))
+    resume_info = ResumeInfo(my_resume, job_post)
+    print(resume.score_and_review_avg(resume_info))
 
     # rewrite
-    print(resume.rewrite(my_resume, job_post))
+    # print(resume.rewrite(resume_info))
